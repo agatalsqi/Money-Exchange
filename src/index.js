6c75038d53849a707f9791b6887b9da699d66dfa
Agatalsqi <agatalsqi@gmail.com>
2018-02-25 01:50:07 +0300

    Solution 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 10000) {
+        if (currency > 0) {
+            var coins = {
+                H: 50,
+                Q: 25,
+                D: 10,
+                N: 5,
+                P: 1
+            };
+                var coinsAmounts = {};
+                var currentNominalAmount = Math.floor(currency / coins['H']);
+                var coinsRemainder = currency % coins['H'];
+                if (currentNominalAmount){
+                    coinsAmounts.H = currentNominalAmount;
+                }
+                if (coinsRemainder) {
+                    currentNominalAmount = Math.floor(coinsRemainder / coins['Q']);
+                    coinsRemainder = coinsRemainder % coins['Q'];
+                    if (currentNominalAmount) {
+                        coinsAmounts.Q = currentNominalAmount;
+                    }
+                    if (coinsRemainder) {
+                        currentNominalAmount = Math.floor(coinsRemainder / coins['D']);
+                        coinsRemainder = coinsRemainder % coins['D'];
+                        if (currentNominalAmount) {
+                            coinsAmounts.D = currentNominalAmount;
+                        }
+                        if (coinsRemainder) {
+                            currentNominalAmount = Math.floor(coinsRemainder / coins['N']);
+                            coinsRemainder = coinsRemainder % coins['N'];
+                            if (currentNominalAmount) {
+                                coinsAmounts.N = currentNominalAmount;
+                            }
+                            if (coinsRemainder) {
+                                currentNominalAmount = Math.floor(coinsRemainder / coins['P']);
+                                if (currentNominalAmount){
+                                    coinsAmounts.P = currentNominalAmount;
+                                } 
+                            }
+                        }
+                    }
+                }
+                console.log(coinsAmounts);
+                return coinsAmounts;
+            }
+            else {
+                return {};
+            }
+        }
+    else {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
 }
